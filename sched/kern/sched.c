2bea9f24bc73c234b8dc8cdf38ea813926057cce
Lara Converso <63512648+Laraconverso@users.noreply.github.com>
2024-10-22 21:16:12 -0300

    agrego sched

---
+++ sched/kern/sched.c
@@ -0,0 +1,129 @@
+#include <inc/assert.h>
+#include <inc/x86.h>
+#include <kern/spinlock.h>
+#include <kern/env.h>
+#include <kern/pmap.h>
+#include <kern/monitor.h>
+
+void sched_halt(void);
+void sched_rr(void);
+
+// Choose a user environment to run and run it.
+void
+sched_yield(void)
+{
+#ifdef SCHED_ROUND_ROBIN
+	// Implement simple round-robin scheduling.
+	//
+	// Search through 'envs' for an ENV_RUNNABLE environment in
+	// circular fashion starting just after the env this CPU was
+	// last running. Switch to the first such environment found.
+	//
+	// If no envs are runnable, but the environment previously
+	// running on this CPU is still ENV_RUNNING, it's okay to
+	// choose that environment.
+	//
+	// Never choose an environment that's currently running on
+	// another CPU (env_status == ENV_RUNNING). If there are
+	// no runnable environments, simply drop through to the code
+	// below to halt the cpu.
+
+	sched_rr();
+#endif
+
+#ifdef SCHED_PRIORITIES
+	// Implement simple priorities scheduling.
+	//
+	// Environments now have a "priority" so it must be consider
+	// when the selection is performed.
+	//
+	// Be careful to not fall in "starvation" such that only one
+	// environment is selected and run every time.
+
+	// Your code here - Priorities
+#endif
+
+	// Without scheduler, keep runing the last environment while it exists
+	if (curenv) {
+		env_run(curenv);
+	}
+
+	// sched_halt never returns
+	sched_halt();
+}
+
+
+void
+sched_rr()
+{
+	// Inicializo start según el entorno actual. Si hay un proceso en ejecución
+	// con ENVX recibo el indice del actual y le sumo 1 para buscar el proximo sino en 0
+	int start = curenv ? ENVX(curenv->env_id) + 1 : 0;
+	// corro el for NENV veces ya que es el numero total de entornos disponibles
+	for (int i = 0; i < NENV; i++) {
+		// calculo el indice revisando todos los entornos a partir de
+		// start y si llega al final vuelve al principio (cirucular)
+		int index = (start + i) % NENV;
+		if (envs[index].env_status == ENV_RUNNABLE) {
+			// aca termina el for porque el env_run toma el control del entorno
+			env_run(&envs[index]);
+		}
+	}
+	// si no se encuentra ningun proceso con la prioridad pasada
+	// miro el status actual. Si es running, continuo ejecutando
+	if (curenv && curenv->env_status == ENV_RUNNING) {
+		env_run(curenv);
+	}
+	// Si no encuentro nada, detengo la CPU.
+	sched_halt();
+}
+
+// Halt this CPU when there is nothing to do. Wait until the
+// timer interrupt wakes it up. This function never returns.
+//
+void
+sched_halt(void)
+{
+	int i;
+
+	// For debugging and testing purposes, if there are no runnable
+	// environments in the system, then drop into the kernel monitor.
+	for (i = 0; i < NENV; i++) {
+		if ((envs[i].env_status == ENV_RUNNABLE ||
+		     envs[i].env_status == ENV_RUNNING ||
+		     envs[i].env_status == ENV_DYING))
+			break;
+	}
+	if (i == NENV) {
+		cprintf("No runnable environments in the system!\n");
+		while (1)
+			monitor(NULL);
+	}
+
+	// Mark that no environment is running on this CPU
+	curenv = NULL;
+	lcr3(PADDR(kern_pgdir));
+
+	// Mark that this CPU is in the HALT state, so that when
+	// timer interupts come in, we know we should re-acquire the
+	// big kernel lock
+	xchg(&thiscpu->cpu_status, CPU_HALTED);
+
+	// Release the big kernel lock as if we were "leaving" the kernel
+	unlock_kernel();
+
+	// Once the scheduler has finishied it's work, print statistics on
+	// performance. Your code here
+
+	// Reset stack pointer, enable interrupts and then halt.
+	asm volatile("movl $0, %%ebp\n"
+	             "movl %0, %%esp\n"
+	             "pushl $0\n"
+	             "pushl $0\n"
+	             "sti\n"
+	             "1:\n"
+	             "hlt\n"
+	             "jmp 1b\n"
+	             :
+	             : "a"(thiscpu->cpu_ts.ts_esp0));
+}
---
+++ sched/sched.md
@@ -0,0 +1,27 @@
+# sched
+
+Lugar para respuestas en prosa, seguimientos con GDB y documentación del TP.
+
+# Parte 1 - Cambio de contexto 
+Utilizar GDB para visualizar el cambio de contexto. Realizar una captura donde se muestre claramente:
+- el cambio de contexto
+- el estado del stack al inicio de la llamada de context_switch
+- cómo cambia el stack instrucción a instrucción
+- cómo se modifican los registros luego de ejecutar iret
+
+<img width="800" alt="1" src="https://github.com/user-attachments/assets/9508a498-c3f2-488c-a4fb-6ac05aba3b77">
+
+<img width="800" alt="2" src="https://github.com/user-attachments/assets/980b0801-125f-4c2b-93e1-39d8b70520bc">
+
+<img width="800" alt="3" src="https://github.com/user-attachments/assets/ec077b1b-b8b7-42cd-980e-55ee65697385">
+
+<img width="800" alt="4" src="https://github.com/user-attachments/assets/53485819-c013-481a-937a-da5f270dcaee">
+
+<img width="800" alt="5" src="https://github.com/user-attachments/assets/4f61222b-b706-40aa-82ad-2daeb2cc7d8b">
+
+
+
+# Parte 2 - Round Robin
+
+
+# Parte 3 - Priorities
